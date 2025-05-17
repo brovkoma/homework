b9ee3fd95408dd01c39c51c36b65ea760dce9649
brovkoma <brovkoma18@rambler.ru>
2025-05-17 18:54:05 +0400

    added extra task on graphs: shortest paths in weighted graphs using Ford-Bellman algorithm

---
+++ C++/30 weighted graph/task30.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    cout << "Введите число вершин и число ребер" << endl;
+
+    int n, m;
+    cin >> n >> m;
+
+    cout << "Введите ребра" << endl;
+
+    vector <int> U, V, W; // начала, концы и веса ребер
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w; // концы ребра и вес
+        cin >> u >> v >> w;
+
+        u--; // в программе будем нумеровать ребра с 0, а во вводе с 1
+        v--;
+
+        U.push_back(u);
+        V.push_back(v);
+        W.push_back(w);
+
+        swap(u, v); // граф неориентированный, поэтому начало и конец могут быть поменяны местами
+
+        U.push_back(u);
+        V.push_back(v);
+        W.push_back(w);
+    }
+
+    vector <int> d(n, 1e9); // расстояния от стартовой вершины до всех остальных
+
+    cout << "Введите стартовую вершину" << endl;
+
+    int start;
+    cin >> start;
+    start--;
+
+    d[start] = 0;
+
+    // алгоритм Форда-Беллмана
+
+    for (int i = 0; i < n - 1; i++) { // n - 1 раз перебираем все ребра
+        for (int j = 0; j < U.size(); j++) {
+            d[V[j]] = min(d[V[j]], d[U[j]] + W[j]); // обновляем расстояние до конца ребра через расстояние до начала
+        }
+    }
+
+    cout << "Расстояния до всех вершин: ";
+
+    for (int i = 0; i < n; i++) {
+        cout << d[i] << ' ';
+    }
+}
